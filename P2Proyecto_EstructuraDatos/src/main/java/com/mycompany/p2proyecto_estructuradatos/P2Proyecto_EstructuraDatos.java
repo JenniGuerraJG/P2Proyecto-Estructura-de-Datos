04f2c3a2cc3006502291f6ef15339fe7bdcc7513
JenniGuerraJG <150312014+JenniGuerraJG@users.noreply.github.com>
2024-07-25 20:44:14 -0500

    Add files via upload

---
+++ P2Proyecto_EstructuraDatos/src/main/java/com/mycompany/p2proyecto_estructuradatos/P2Proyecto_EstructuraDatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p2proyecto_estructuradatos;
+
+/**
+ *
+ * @author jenni
+ */
+public class P2Proyecto_EstructuraDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
